df6ff9d7b82bbe282e95daae06877346db2c58d0
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-12-06 03:05:13 +0000

    Decomp some of makerom.c

--- src/makerom.c
+++ src/makerom.c
@@ -2,21 +2,39 @@
 
 static const sigaction_t act;
 
-static unsigned char segmentSymbolSource[255];
+// static unsigned char segmentSymbolSource[255];
 
-static unsigned char segmentSymbolObject[255];
+// static unsigned char segmentSymbolObject[255];
 
-static unsigned char entrySource[255];
+// static unsigned char entrySource[255];
 
-static unsigned char entryObject[255];
+// static unsigned char entryObject[255];
 
-static unsigned char objectListFile[255];
+// static unsigned char objectListFile[255];
 
-static unsigned char* romFile;
+static unsigned char romFile[] = "rom";
 
-static int checkOverlap;
+// static int checkOverlap;
 
-static unsigned char* progName;
+// static unsigned char* progName;
+
+static char B_1000B540[0x100]; // segmentSymbolSource
+static char B_1000B640[0x100]; // segmentSymbolObject
+static char B_1000B740[0x100]; // entrySource
+static char B_1000B840[0x100]; // entryObject
+static char B_1000B940[0x100]; // objectListFile
+
+static char* B_1000BA40; // progName
+
+extern int debug;
+extern int keep_going;
+extern Wave* waveList;
+extern size_t* bootBuf;
+extern int bootWordAlignedByteSize;
+
+
+
+
 
 #pragma GLOBAL_ASM("asm/functions/makerom/main.s")
 int main(int argc, unsigned char** argv);
@@ -42,20 +60,57 @@ int main(int argc, unsigned char** argv);
 //    }
 //}
 
-#pragma GLOBAL_ASM("asm/functions/makerom/usage.s")
+// #pragma GLOBAL_ASM("asm/functions/makerom/usage.s")
 void usage();
 //{
 //}
+void usage(void) {
+    fprintf(stderr, "usage: makerom [-D<define>] [-I<dir>] -[U<define>]\n");
+    fprintf(stderr, "               [-c] [-n] [-d] [-g] [-e] [-m] [-q] [-v]\n");
+    fprintf(stderr, "               [-o] [-b bootfile] [-h headerfile]\n");
+    fprintf(stderr, "               [-p pif2bootfile]\n");
+    fprintf(stderr, "               [-s romsize (Mbits)]\n");
+    fprintf(stderr, "               [-f filldata (0x00 - 0xff)]\n");
+    fprintf(stderr, "               [-C clockrate] \n");
+    fprintf(stderr, "               [-r romfile] specfile\n");
+}
 
-#pragma GLOBAL_ASM("asm/functions/makerom/usage.s")
+extern int irixVersion;
+
+// #pragma GLOBAL_ASM("asm/functions/makerom/getOsVersion.s")
 void getOsVersion();
 //{
 //    unsigned char* cmd;
 //    unsigned char buf[4096];
 //    FILE* procPtr;
 //}
+void getOsVersion(void) {
+    char* cmd = "/sbin/uname -r";
+    char buf[0x1000];
+    FILE* procPtr;
 
-#pragma GLOBAL_ASM("asm/functions/makerom/checkIdoVersion.s")
+    if ((procPtr = popen(cmd, "r")) != NULL) {
+        fgets(buf, 0x1000, procPtr);
+        pclose(procPtr);
+        if (strcmp(buf, "5.3\n") == 0) {
+            irixVersion = 0;
+        } else if (strcmp(buf, "6.2\n") == 0) {
+            irixVersion = 1;
+        } else if (strcmp(buf, "6.3\n") == 0) {
+            irixVersion = 2;
+        } else if (strcmp(buf, "6.4\n") == 0) {
+            irixVersion = 3;
+        } else {
+            irixVersion = 4;
+            fprintf(stderr, "makerom: Operating system not recognized.  Trying 6.x ...\n");
+        }
+    } else {
+        fprintf(stderr, "makerom: Unable to find uname command!\n");
+        exit(1);
+    }
+}
+
+// #pragma GLOBAL_ASM("asm/functions/makerom/checkIdoVersion.s")
 int checkIdoVersion(unsigned char* rootName);
 //{
 //    int u64CheckFound;
@@ -65,13 +120,65 @@ int checkIdoVersion(unsigned char* rootName);
 //    stat statBuffer;
 //    FILE* procPtr;
 //}
+int checkIdoVersion(unsigned char* rootName) {
+    int u64CheckFound;
+    int v70Found; // Version 7.0
+    char cmd[0x100];
+    char buffer[0x100];
+    struct stat statBuffer;
+    FILE* procPtr;
 
-#pragma GLOBAL_ASM("asm/functions/makerom/printVersion.s")
+    sprintf(buffer, "%s/usr/sbin/u64check", rootName);
+    if (stat(buffer, &statBuffer) != 0) {
+        u64CheckFound = 0;
+    } else {
+        u64CheckFound = 1;
+    }
+    
+    // Check if the fourth line of the output of this command contains "7.0"
+    sprintf(cmd, "/usr/sbin/showprods -D 1 dev");
+    if ((procPtr = popen(cmd, "r")) != NULL) {
+        fgets(buffer, 0xFF, procPtr);
+        fgets(buffer, 0xFF, procPtr);
+        fgets(buffer, 0xFF, procPtr);
+        fgets(buffer, 0xFF, procPtr);
+        pclose(procPtr);
+        if (strstr(buffer, "7.0") != NULL) {
+            v70Found = 1;
+        } else {
+            v70Found = 0;
+        }
+    }
+
+    if (v70Found) {
+        fprintf(stderr, "makerom: IDO v7.0 does not work with the Nintendo64\n");
+        fprintf(stderr, "         development environment.  Please upgrade to\n");
+        fprintf(stderr, "         IDO v7.1.\n");
+        exit(1);
+    }
+
+    if (u64CheckFound) {
+        return 2;
+    } else {
+        return 0;
+    }
+}
+
+// #pragma GLOBAL_ASM("asm/functions/makerom/printVersion.s")
 void printVersion();
 //{
 //}
+void printVersion(void) {
+    if (irixVersion == 0) {
+        printf("Nintendo64 Makerom v2.2 for IRIX.\n");
+    } else {
+        printf("Nintendo64 Makerom v2.2 -BETA- for IRIX.\n");
+    }
+}
 
-#pragma GLOBAL_ASM("asm/functions/makerom/getBootFile.s")
+int readCoff(unsigned char* fname, unsigned int* buf);
+
+// #pragma GLOBAL_ASM("asm/functions/makerom/getBootFile.s")
 void getBootFile(unsigned char* bootFileName);
 //{
 //    int bootFd;
@@ -79,6 +186,41 @@ void getBootFile(unsigned char* bootFileName);
 //    stat buf;
 //    unsigned char errMessage[255];
 //}
+void getBootFile(unsigned char* bootFileName) {
+    int bootFd;
+    char scratchFileName[0x100];
+    struct stat buf;
+    char errMessage[0x100];
+
+    if ((bootFileName == NULL) && (gloadFindFile(scratchFileName, "/usr/lib/PR", "Boot") != NULL)) {
+        bootFileName = scratchFileName;
+    }
+    if (bootFileName != NULL) {
+        if ((bootFd = open(bootFileName, 0x800)) < 0) {
+            sprintf(errMessage, "%s: unable to open %s", B_1000BA40, bootFileName);
+            perror(errMessage);
+            exit(1);
+        }
+        if (fstat(bootFd, &buf) < 0) {
+            sprintf(errMessage, "%s unable to stat %s", B_1000BA40, bootFileName);
+            perror(errMessage);
+            close(bootFd);
+            exit(1);
+        }
+
+        bootBuf = malloc(buf.st_size);
+        if (bootBuf == NULL) {
+            fprintf(stderr, "%s: unable to malloc buffer to hold %d bytes\n", B_1000BA40, buf.st_size);
+            close(bootFd);
+            exit(1);
+        }
+        close(bootFd);
+        bootWordAlignedByteSize = readCoff(bootFileName, bootBuf);
+    } else {
+        bootBuf = NULL;
+    }
+}
+
 
 #pragma GLOBAL_ASM("asm/functions/makerom/getPif2BootFile.s")
 void getPif2BootFile(unsigned char* pif2bootFileName);
@@ -121,31 +263,111 @@ unsigned char* gloadFindFile(unsigned char* fullpath, unsigned char* postRootSuf
 //    int try;
 //}
 
-#pragma GLOBAL_ASM("asm/functions/makerom/doWave.s")
+int createElspec(Wave*);
+int runLinker(Wave*, unsigned char*, unsigned char*);
+
+// #pragma GLOBAL_ASM("asm/functions/makerom/doWave.s")
 void doWave(Wave* wave);
 //{
 //}
+void doWave(Wave* wave) {
+    if (debug) {
+        printf("Translating ROM spec file into");
+        printf(" linker spec file in %s\n", wave->elspecFile);
+    }
+    if (createElspec(wave) == -1) {
+        unlinkTempFiles();
+        exit(1);
+    }
+    if ((runLinker(wave, B_1000B640, B_1000B940) == -1) && !keep_going) {
+        unlinkTempFiles();
+        exit(1);
+    }
+}
 
-#pragma GLOBAL_ASM("asm/functions/makerom/nameTempFiles.s")
+
+// #pragma GLOBAL_ASM("asm/functions/makerom/nameTempFiles.s")
 void nameTempFiles();
 //{
 //    Wave* wave;
 //    unsigned char* tmpdir;
 //}
+void nameTempFiles(void) {
+    Wave* wave;
+    char* tmpdir;
 
-#pragma GLOBAL_ASM("asm/functions/makerom/unlinkTempFiles.s")
+    if ((tmpdir = getenv("TMPDIR")) == NULL) {
+        tmpdir = "/tmp";
+    }
+    for (wave = waveList; wave != NULL; wave = wave->next) {
+        sprintf(wave->elspecFile, "%s/%sElspecXXXXXX", tmpdir, wave->name);
+        mktemp(wave->elspecFile);
+    }
+
+    sprintf(B_1000B540, "%s/segmentXXXXXX", tmpdir);
+    mktemp(B_1000B540);
+    strcpy(B_1000B640, B_1000B540);
+    strcat(B_1000B540, ".s");
+    strcat(B_1000B640, ".o");
+
+    sprintf(B_1000B740, "%s/entryXXXXXX", tmpdir);
+    mktemp(B_1000B740);
+    strcpy(B_1000B840, B_1000B740);
+    strcat(B_1000B740, ".s");
+    strcat(B_1000B840, ".o");
+
+    sprintf(B_1000B940, "%s/objListXXXXXX", tmpdir);
+    mktemp(B_1000B940);
+}
+
+// #pragma GLOBAL_ASM("asm/functions/makerom/unlinkTempFiles.s")
 static void unlinkTempFiles();
 //{
 //    Wave* wave;
 //}
+static void unlinkTempFiles(void) {
+    Wave* wave;
 
-#pragma GLOBAL_ASM("asm/functions/makerom/cleanup.s")
+    if (!debug) {
+        for (wave = waveList; wave != NULL; wave = wave->next) {
+                unlink(wave->elspecFile);
+        }
+        unlink(B_1000B540);
+        unlink(B_1000B640);
+        unlink(B_1000B740);
+        unlink(B_1000B840);
+        unlink(B_1000B940);
+    }
+}
+
+// #pragma GLOBAL_ASM("asm/functions/makerom/cleanup.s")
 void cleanup(int sig);
 //{
 //}
+void cleanup(int sig) {
+    unlinkTempFiles();
+    exit(1);
+}
 
-#pragma GLOBAL_ASM("asm/functions/makerom/execCommand.s")
+// #pragma GLOBAL_ASM("asm/functions/makerom/execCommand.s")
 int execCommand(unsigned char* s);
 //{
 //    int status;
 //}
+int execCommand(unsigned char* s) {
+    int status = system(s);
+
+    if (status == -1) {
+        fprintf(stderr, "makerom: cannot execute command: %s\n", sys_errlist[errno]);
+        return -1;
+    } else if (WIFEXITED(status) && (WEXITSTATUS(status) == 0)) {
+        return 0;
+    } else if (keep_going && WIFEXITED(status) && (WEXITSTATUS(status) == 1)) {
+            return 1;
+    } else {
+        if (debug) {
+            fprintf(stderr, "makerom: [%s] returned %d (%08x), errno=%d\n", s, WEXITSTATUS(status), status, errno);
+        }
+        return -1;
+    }
+}
